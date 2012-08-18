29d8d3b3aebf3a25eaaeea938e871f6f7f602daf
Carlos Fonseca <carlosefonseca@gmail.com>
2012-08-18 20:18:00 +0100

    acção não obrigatória

--- myTrain/urls.py
+++ myTrain/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import *
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
 
-from myTrain.handlers import TestThing
 from CP import CP
 from Refer import Refer
 
@@ -11,7 +10,6 @@ ad = {  }
 
 # blogpost_resource = Resource(handler=BlogPostHandler, **ad)
 # arbitrary_resource = Resource(handler=ArbitraryDataHandler, **ad)
-test_thing = Resource(handler=TestThing, **ad)
 cp_resource = Resource(handler=CP, **ad)
 refer_resource = Resource(handler=Refer, **ad)
 
@@ -19,8 +17,7 @@ refer_resource = Resource(handler=Refer, **ad)
 # url(r'^other/(?P<username>[^/]+)/(?P<data>.+)/$', arbitrary_resource), 
  
 urlpatterns = patterns('',
-	(r'^test/(?P<var1>[^/]+)$', test_thing),
-	(r'^cp/(?P<action>[^/?]+)/?$', cp_resource),
-	(r'^refer/(?P<action>[^/?]+)/?$', refer_resource)
+	(r'^cp/(?P<action>[^/?]*)/?$', cp_resource),
+	(r'^refer/(?P<action>[^/?]*)/?$', refer_resource)
 )
 
